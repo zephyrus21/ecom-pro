653df4398a7edee8c2a95337e5e4433487d77dcf
zephyrus21 <pandey.piyus@gmail.com>
2021-11-25 11:52:47 +0530

    user model password cypts

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -49,5 +52,34 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+//! validate the password with the user password
+userSchema.methods.isCorrectPassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+//! generate and return token for user
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+};
+
+//! generate forgot password token
+userSchema.methods.getForgotPasswordToken = function () {
+  //? generate a long random string
+  const token = crypto.randomBytes(20).toString("hex");
+
+  //? getting a hash - make sure to get a hash in backend
+  this.forgotPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  //? set expiry
+  this.forgotPasswordExpiry = Date.now() + 10 * 60 * 1000;
+
+  return token;
+};
 
 module.exports = mongoose.model("User", userSchema);
